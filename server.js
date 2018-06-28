7116a4f8653679efd42a3b7b439a956359f6a4b9
kasia-misirli <kasia.misirli@gmail.com>
2018-06-28 23:02:36 +1000

    added searching for particular flight

--- server.js
+++ server.js
@@ -15,3 +15,30 @@ app.get('/flights',(req,res) => {
   var data = JSON.parse(fs.readFileSync('flights.json').toString())
   res.json(data)
 })
+
+// var origin = ''
+// var destination = ''
+// var year = null
+// var month = null
+// var day = null
+
+app.get(`/flights&origin=:origin&destination=:destination&year=:year&month=:month`,(req,res)=> {
+  var origin = req.params.origin;
+  var destination = req.params.destination;
+  var year = Number(req.params.year)
+  var month = Number(req.params.month)
+  var data = JSON.parse(fs.readFileSync('flights.json').toString())
+  var flightsFound = []
+  var possibleFlights = []
+  for(var flight in data){
+    if(data[flight]["origin"] == origin && data[flight]["destination"] == destination){
+       flightsFound.push(data[flight])
+    }
+  }
+  for(var a in flightsFound){
+    if(flightsFound[a]["year"] == year && flightsFound[a]["month"] == month){
+      possibleFlights.push(flightsFound[a])
+    }
+  }
+  res.json(possibleFlights)
+})
