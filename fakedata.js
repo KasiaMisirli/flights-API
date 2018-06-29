0bd36c5c05acd8441ad0d25fb6b01b4ac9d2d800
kasia-misirli <kasia.misirli@gmail.com>
2018-06-29 11:47:32 +1000

    adding new data

--- fakedata.js
+++ fakedata.js
@@ -1,12 +1,15 @@
 var dataFaker = require("data-faker");
+var fs = require('fs');
  
 dataFaker.set("origin", ["Melbourne", "Sydney", "Perth", "Canberra", "Newcastle", "Brisbane","Adelaide","Gold Coast","Darwin","Hobart"]);
 dataFaker.set("destination", ["Melbourne", "Sydney", "Perth", "Canberra", "Newcastle", "Brisbane","Adelaide","Gold Coast","Darwin","Hobart"]);
 
 dataFaker.set("day",[1,30],{range:true, unique: true});
-dataFaker.set("month",[1,7],{range:true, unique: true});
-dataFaker.set("year",[2019,2019],{range:true, unique: true});
+dataFaker.set("month",[7,12],{range:true, unique: true});
+dataFaker.set("year",[2018,2018],{range:true, unique: true});
 dataFaker.set("price",[69,600], {range: true, unique: true});
+dataFaker.set("hour",[00,23],{range:true, unique: true});
+dataFaker.set("minute",[00,59],{range:true, unique: true});
 
 var flights = dataFaker.schema([{
     origin: "origin",
@@ -15,8 +18,11 @@ var flights = dataFaker.schema([{
     month: "month",
     year: "year",
     price: "price",
-    currency: "AUD"
-}, 100]);
+    currency: "AUD",
+    hour: "hour",
+    minute: "minute"
+}, 1000]);
 
 
 console.log(flights)
+fs.writeFile('flight-data.json', JSON.stringify(flights,null,4));
