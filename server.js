8c45d009a72ce6712655fef07004ed85b968e653
kasia-misirli <kasia.misirli@gmail.com>
2018-07-11 11:18:05 +1000

    refactoring

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const PORT = 7777
 app.listen(PORT,() => {
   console.log(`listening to ${PORT}`)
 })    
-
+//CORS: cross origin resource sharing, tells browser to allow access to resources at different origin/domain 
 app.use(cors())
 //reading file with data
+
+var data = JSON.parse(fs.readFileSync('flight-data.json').toString())//turned into global variable
+
 app.get('/flights',(req,res) => {
-  var data = JSON.parse(fs.readFileSync('flight-data.json').toString())
   res.json(data)
 })
 //setting up the api response with the expected data
@@ -23,27 +25,16 @@ app.get(`/flights&origin=:origin&destination=:destination&year=:year&month=:mont
   var year = Number(req.params.year)
   var month = Number(req.params.month)
   var day = Number(req.params.day)
-  var data = JSON.parse(fs.readFileSync('flight-data.json').toString())
-  var flightsFound = []
-  var possibleFlights = []
-  var bestFlights = []
-  //filtering he data and getting to a closest match
-  for(var flight in data){
-    if(data[flight]["origin"] == origin && data[flight]["destination"] == destination){
-       flightsFound.push(data[flight])
+
+  var bestFlight = []
+ 
+   for(var flight in data){
+    if(data[flight]["origin"] == origin && data[flight]["destination"] == destination && data[flight]   ["year"] == year && data[flight]["month"] == month && data[flight]["day"] > day){
+      bestFlight.push(data[flight])
     }
   }
-  for(var a in flightsFound){
-    if(flightsFound[a]["year"] == year && flightsFound[a]["month"] == month){
-      possibleFlights.push(flightsFound[a])
-    }
-  }
-  for(var b in possibleFlights){
-    if(possibleFlights[b]["day"] > day){
-      bestFlights.push(possibleFlights[b])
-    }
-  } 
-  var byDate = bestFlights.slice(0);
+
+  var byDate = bestFlight.slice(0);
   byDate.sort(function(a,b) {
     return a.day - b.day;
   });
